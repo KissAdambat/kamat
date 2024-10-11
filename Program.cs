84e2b952942b74b0e695aa8ee6eb0a04a34ec1f1
KissAdam <kissa@kkszki.hu>
2024-10-11 12:20:43 +0200

    Ready to use

--- Program.cs
+++ Program.cs
@@ -8,8 +8,25 @@ namespace kamat
 {
     internal class Program
     {
+
+        static void kamat()
+        {
+            Console.WriteLine("Adja meg hány évre szeretné berakni a pénzét: ");
+            int ev = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mekkora összeget szeretne berakni:");
+            double mennyiseg = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Mennyi a kamat?");
+            double kmat = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Kamatozási periódusok: ");
+
+            double vegsoosszeg = mennyiseg * Math.Pow(1 + kmat / 100, ev);
+
+            Console.WriteLine($"{ev} év múlva: {vegsoosszeg:F2}Ft");
+        }
+
         static void Main(string[] args)
         {
+            kamat();
         }
     }
 }
